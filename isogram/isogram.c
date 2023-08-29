076f9599695cdad701cfd582c6c5713aba66f696
Eps (Rarima) <rarimar@outlook.com>
2023-08-29 11:21:32 +0000

    pass

--- isogram/isogram.c
+++ isogram/isogram.c
@@ -1,19 +1,37 @@
 #include "isogram.h"
 
-bool is_isogram(const char phrase[]){
-	if (phrase == NULL)
-	{
-		return false;
-	}
-	int len = strlen(phrase);
-	for(int i = 0; i < len - 1; i++){
-		for (int j = i + 1; j < len; j++)
-		{
-			if (phrase[i] == phrase[j])
-			{
-				return false;
-			}
-		}
-	}
-	return true;
+bool is_isogram(const char phrase[])
+{
+  if (phrase == NULL)
+  {
+    return false;
+  }
+
+  int len = strlen(phrase);
+  char seen[26] = {0};
+
+  for (int i = 0; i < len; i++)
+  {
+    char c = phrase[i];
+
+    if (islower(c))
+    {
+      if (seen[c - 'a'] != 0)
+      {
+        return false;
+      }
+      seen[c - 'a'] = 1;
+    }
+
+    else if (isupper(c))
+    {
+      if (seen[c - 'A'] != 0)
+      {
+        return false;
+      }
+      seen[c - 'A'] = 1;
+    }
+  }
+
+  return true;
 }
